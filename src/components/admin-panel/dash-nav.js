9ad6eefa37397e230865faed198a6645abc6223c
Dawn Sheedy <sheedy.dawn@gmail.com>
2020-07-10 20:07:16 -0400

    Beginning of page switcher implementation on admin dashboard.
    Structured similarly to the root page switcher.

--- src/components/admin-panel/dash-nav.js
+++ src/components/admin-panel/dash-nav.js
@@ -4,25 +4,30 @@ import { Nav } from 'react-bootstrap'
 export default class DashNav extends Component {
     render() {
         return (
-            <Nav variant="tabs" defaultActiveKey="/survey">
-                <h5>Hello, {this.props.name}</h5>
-                <Nav.Item>
-                    <Nav.Link eventKey="survey" href="#/survey">Surveys</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="invite">Invites</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="question">
-                        Questions
+            <div style={{ padding: '10px' }}>
+                <Nav variant="pills" defaultActiveKey="/survey">
+                    <Nav.Item>
+                        <Nav.Link disabled><h4>Hello, {this.props.name}</h4></Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="survey" onClick={this.props.setPage("survey")} href="#/survey">Surveys</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="invite" onClick={this.props.setPage("invite")}>Invites</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="question" onClick={this.props.setPage("question")}>
+                            Questions
                     </Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="answer">
-                        Answers
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="answer" onClick={this.props.setPage("answer")}>
+                            Answers
                     </Nav.Link>
-                </Nav.Item>
-            </Nav>
+                    </Nav.Item>
+                </Nav>
+                <hr />
+            </div>
         )
     }
 }
--- src/components/admin-panel/router.js
+++ src/components/admin-panel/router.js
@@ -1,23 +1,29 @@
 import { connect } from 'react-redux'
 import React, { Component } from 'react'
 import DashNav from './dash-nav'
+import { Transition } from 'react-spring/renderprops'
 
 class AdminRouter extends Component {
-constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.state = {
-        survey: "",
-        invite: "",
-        question: "",
-        answer: ""
+        this.state = {
+            survey: "",
+            invite: "",
+            question: "",
+            answer: "",
+            page: ""
+        }
     }
-}
+
+    setPage(page) {
+        this.setState({ page })
+    }
 
     setSurvey(survey) {
         this.setState({ survey })
     }
-    
+
     setInvite(invite) {
         this.setState({ invite })
     }
@@ -30,9 +36,26 @@ constructor(props) {
         this.setState({ answer })
     }
 
+    renderSection() {
+
+    }
+
     render() {
         return (<div>
-            <DashNav name={this.props.user.firstName}/>
+            <DashNav setPage={this.setPage} name={this.props.user.firstName} />
+            <Transition
+                config={{ mass: 1, tension: 250, friction: 25 }}
+                items={this.state.page}
+                from={{ height: '0', opacity: 0, transform: 'translate3d(0,-50%,0)' }}
+                enter={{ height: '100%', opacity: 1, transform: 'translate3d(0,0px,0)' }}
+                leave={{ height: '0%', opacity: 0, transform: 'translate3d(0,50%,0)' }}
+            >
+                {mode =>
+                    props => <div style={{ ...props, ...{ padding: '20px' } }}>
+                        
+                    </div>
+                }
+            </Transition>
         </div>)
     }
 }
